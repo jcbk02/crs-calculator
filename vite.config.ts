9498525117ec17f099de2c217e38b7a172bc89c6
Je Ho Cho <baneofkermit@gmail.com>
2025-11-24 09:57:28 -0500

    Blank screen fix on deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+const repoName = 'crs-calculator'; 
+
 export default defineConfig({
   plugins: [react()],
-})
+  
+ 
+  base: `/${crs-calculator}/`, 
+});
