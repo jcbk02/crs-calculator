815dbf43098d9e10a67a6a07f02b6f83e648a004
Je Ho Cho <baneofkermit@gmail.com>
2025-11-24 10:01:02 -0500

    Again

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const repoName = 'crs-calculator'; 
-
 export default defineConfig({
   plugins: [react()],
+
+  base: '/crs-calculator/', 
   
- 
-  base: `/${crs-calculator}/`, 
 });
